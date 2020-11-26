c5cb2e0d51c3d1c56a4498b307147b1c983f1832
MurphyStudebaker <murphy.stude@gmail.com>
2020-11-25 19:05:03 -0800

    fixed mobile bug

--- pages/tools.js
+++ pages/tools.js
@@ -3,12 +3,12 @@ import Footer from '../components/footer'
 
 export default function Tools() {
     return (
-    <div className='min-h-screen rainbow  flex flex-col justify between'>
+    <div className='min-h-screen max-w-screen rainbow  flex flex-col justify between'>
       <nav className="flex justify-between px-10 py-10">
         <Link href='/'><p className='text-6x text-white font-bold mono cursor-pointer'>{`<🏳️‍🌈build with pride/>`}</p></Link>
         <Link href='/tools'><p  className='text-white font-bold mono cursor-pointer'>tools</p></Link>
       </nav>
-      <div className='flex-grow mt-10 container mx-auto'>
+      <div className='flex-grow mt-10 container mx-auto px-10'>
         <h2 className='text-white mono text-2xl'>Social Cover Photos</h2>
         <a href="https://photos.app.goo.gl/t9h6yGVbV5XesMoy8" className="text-white mono underline">Download on Google Photos</a>
       
@@ -16,7 +16,7 @@ export default function Tools() {
         <p className='text-white mono'>Just throw this at the bottom of your footer :)</p>
         <div className='border border-white rounded px-4 py-4 mt-2'>
           <p className='text-white mono'>
-          {`<a href='http://www.buildwithpride.org'>Built with 🏳️‍🌈</a>`}
+          {`<a href='http://www.buildwithpride.org'> Built with 🏳️‍🌈 </a>`}
           </p>
         </div>
       </div>
