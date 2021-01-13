b998a77bd5c06ef0047f81861f74d6b86fc442bd
Murphy Studebaker <murphystudebaker@macbook-pro.lan>
2021-01-12 18:41:04 -0800

    changed email to discord link bc nobody wants another email form

--- components/Email.js
+++ components/Email.js
@@ -1,18 +1,8 @@
 export default function Email() {
     return (
-      <form className='mt-4 w-full' name="contact" action="/" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact" />
-        <div className='flex flex-col items-center w-full px-4'>
-        <p className='w-full text-lg'>
-          <input className='px-8 py-6 rounded-lg w-full mono' type="email" name="email" placeholder='Your Email' id="youremail" />
-        </p>
-        <p className='w-full text-lg'>
-          <button className='mt-2 bg-transparent text-white py-6 px-6 border-4 border-white rounded-lg w-full mono hover:shadow-xl' type="submit">
-          Join the gays!
-          </button>
-        </p>
-
-        </div>
-      </form>
+      
+          <a href="https://discord.gg/KVaxaSMnM8" className='mt-2 mx-2 bg-transparent text-white py-6 px-6 border-4 border-white rounded-lg w-3/4 mono hover:shadow-xl' type="submit">
+          Say hi on our discord server!
+          </a>
     )
   }
